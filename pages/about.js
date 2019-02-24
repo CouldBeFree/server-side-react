55ae680895878fd28de682c73e24d0815a70f955
Explorer <propokerstar90@gmail.com>
2019-02-24 20:40:20 +0200

    Made About stateful component

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,25 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
+import { Component } from 'react';
+import fetch from 'isomorphic-unfetch';
 
-const About = () => (
-    <Layout title="About">
-        <p>I am the JavaScript developer</p>
-    </Layout>
-);
+export default class About extends Component {
+    static async getInitialProps() {
+        const res = await fetch('https://api.github.com/users/couldbefree');
+        const data = await res.json();
 
-export default About
+        return {
+            user: data
+        }
+    }
+    render () {
+        const { user } = this.props;
+
+        return (
+            <Layout title="About">
+                <img src={user.avatar_url} alt="name" height="200px"/>
+                <p>I am javaScript programmer</p>
+            </Layout>
+        )
+    }
+}
