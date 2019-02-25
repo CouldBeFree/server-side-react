3e3df5cb613056ad36de687a9b381192df1fee60
Sasha Zoria <alex.zoria@n3wnormal.com>
2019-02-25 10:48:06 +0200

    Created error page

---
+++ pages/_error.js
@@ -0,0 +1,7 @@
+import Layout from '../components/Layout';
+
+export default () => (
+    <Layout>
+        <p>This page can't be found</p>
+    </Layout>
+)
--- pages/about.js
+++ pages/about.js
@@ -1,23 +1,30 @@
-import Link from 'next/link';
 import Layout from '../components/Layout';
 import { Component } from 'react';
 import fetch from 'isomorphic-unfetch';
+import Error from '../pages/_error';
 
 export default class About extends Component {
     static async getInitialProps() {
-        const res = await fetch('https://api.github.com/users/couldbefree');
+        const res = await fetch('https://api.github.com/users/reedbarger');
         const data = await res.json();
+        const statusCode = res.status > 200 ? res.status : null;
 
         return {
-            user: data
+            user: data,
+            statusCode
         }
     }
 
     render () {
-        const { user } = this.props;
+        const { user, statusCode } = this.props;
+
+        if(statusCode) {
+            return <Error/>
+        }
 
         return (
             <Layout title="About">
+                <p>{user.name}</p>
                 <img src={user.avatar_url} alt="name" height="200px"/>
                 <p>I am javaScript programmer</p>
             </Layout>
